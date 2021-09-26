9bc075a5efa1388f69755ce046dd50a980a5ecfb
huunghi <nghinguyen08022000@gmail.com>
2021-09-26 19:42:35 +0700

    'abc'

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from numpy import array
 import pandas as pd
 import  glob
 import  numpy as np
-from os import listdir
 from matplotlib import image
+import sys
+import os
+import csv
+import cv2
 # mlr = MultipleLinearRegression()
 #
 # # fit our LR to our data
@@ -24,67 +27,60 @@ from matplotlib import image
 # print('=================================================')
 #
 #
-# sk_mlr = LinearRegression()
-#
-# # fit scikit-learn's LR to our data
-# sk_mlr.fit(x_train, y_train)
-# print(sk_mlr.coef_)
-# print(sk_mlr.intercept_)
-# # predicts and scores
-# sk_score = sk_mlr.score(x_test, y_test)
-# print(f'Scikit-Learn\'s Final R^2 score: {sk_score}')
-# # Press the green button in the gutter to run the script.
-#
-# predictions = sk_mlr.predict(x_test)
-# print('MSE:', metrics.mean_squared_error(y_test, predictions))
-# print('Du doan:', sk_mlr.predict([[62,0,140,268,0,160,0,3.6,0,2]]))
-from matplotlib.image import imread
-img1 = Image.open('stare/1/im0007.jpg')
-ar = np.array(img1)
-print(ar)
-# for row in ar:
-#     outputstring=""
-#     for column in row:
-#         valuestring=""
-#         for value in column:
-#               if value < 10:
-#                   valuestring+="00"+str(value)
-#               elif value < 100:
-#                   valuestring+="0"+str(value)
-#               else:
-#                   valuestring+=str(value)
-#         outputstring+=valuestring+";"
-#     print(outputstring[:-1]) #excluding the last letter, because its a ;
-import csv
+sk_mlr = LinearRegression()
 
-# def csvWriter(fil_name, nparray):
-#   example = nparray.tolist()
-#   with open(fil_name+'.csv', 'w', newline='') as csvfile:
-#     writer = csv.writer(csvfile, delimiter=',')
-#     writer.writerows(example)
-#
-# csvWriter("myfilename", ar)
+# fit scikit-learn's LR to our data
+sk_mlr.fit(x_train, y_train)
+print(sk_mlr.coef_)
+print(sk_mlr.intercept_)
+# predicts and scores
+sk_score = sk_mlr.score(x_test, y_test)
+print(f'Scikit-Learn\'s Final R^2 score: {sk_score}')
+# Press the green button in the gutter to run the script.
 
-# print('========================================================')
-#
-# filenames = glob.glob('stare/1/*.jpg')
-# images = [Image.open(fn).convert('L') for fn in filenames]
-# data = np.stack([np.array(im) for im in images])
-# print(data)
+predictions = sk_mlr.predict(x_test)
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('Du doan:', sk_mlr.predict([[62,0,140,268,0,160,0,3.6,0,2]]))
 
-from matplotlib.pyplot import imread
-import numpy as np
-import pandas as pd
-import os
-# import imageio
-import glob
-import pathlib
-# v = []
-# for i,files in enumerate(pathlib.Path('./Stare/').glob('./**/*.jpg')):
-#     im = imread(files.as_posix())
-#     value = im.flatten()
-#     value = np.hstack((int(files.parent.name),value))
-#     v.append(value)
-# df = pd.DataFrame(v)
-# df = df.sample(frac=1)
-# df.to_csv('files_path.csv',header=False,index=False)
+def createFileList(myDir, format='.jpg'):
+    fileList = []
+    print(myDir)
+    for root, dirs, files in os.walk(myDir, topdown=False):
+        for name in files:
+            if name.endswith(format):
+                fullName = os.path.join(root, name)
+                fileList.append(fullName)
+    return fileList
+
+
+mydir = 'stare_train'
+
+# load the original image
+myFileList = createFileList(mydir)
+
+for file in myFileList:
+    print(file)
+    img_file = Image.open(file)
+    print(np.array(img_file))
+
+
+# X = []
+# y = []
+#
+# data_folder = "stare"
+# for folder in os.listdir(data_folder):
+#     curr_path = os.path.join(data_folder,folder)
+#     for file in os.listdir(curr_path):
+#         curr_file = os.path.join(curr_path,file)
+#         image = cv2.imread(curr_file)
+#         X.append(image)
+#         y.append(folder)
+# print(X)
+# print(y)
+
+# img1 = Image.open('stare/1/im0007.jpg')
+# ar = np.array(img1)
+# print(ar)
+# print("=========================================")
+# img2 = cv2.imread('stare/1/im0007.jpg')
+# print(img2)
